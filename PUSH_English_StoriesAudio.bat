8cf90e37a603030061c82b14bf59b348a0e276db
Kevin DeLand <kevin.deland@gmail.com>
2019-04-09 10:11:29 -0400

    added "pause" statement to batch files

--- PUSH_English_StoriesAudio.bat
+++ PUSH_English_StoriesAudio.bat
@@ -1,1 +1,3 @@
 java -jar RTAssetManager.jar cmd=build_dist:src=English_StoriesAudio:compress=false
+
+pause
--- ZIP_English_StoriesAudio.bat
+++ ZIP_English_StoriesAudio.bat
@@ -1,1 +1,4 @@
 java -jar RTAssetManager.jar cmd=build_dist:src=English_StoriesAudio:compress=true
+
+pause
+
